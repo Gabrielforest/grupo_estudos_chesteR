65e518e1a1b5fe88fe9c39ddfd7fc22683f5f839
Gabrielforest <gabrielfreitaspereira@estudante.ufscar.br>
2021-08-15 17:49:32 -0300

    atualizando escopo do que será discutido no primeiro encontro

---
+++ reuniões/encontro_01_Rstudio_operações.R
@@ -0,0 +1,94 @@
+##############################
+#     chesteR - reunião 01   #
+##############################
+
+
+# Funcionalidades Rstudio -------------------------------------------------
+
+
+#file -> Reopen with enconding -> UTF-8 -> set as default 
+
+
+#edit (atalhos de digitação)
+
+
+#code -> ctrl + shift + R
+
+
+#view -> panes -> Pane Layout
+
+
+#plots - só fica disponível quando faço um gráfico:
+
+plot(c(1:10))
+
+
+#session -> set working directory -> choose directory
+
+
+#build - cria arquivos padrão em determinada tarefa
+
+
+#debug - forma como retorna o erro
+
+
+#profile - como a memória ta sendo usada
+
+
+#tools - usar comandos do github e do prompt de comando abrindo um terminal
+
+
+#help -> cheatsheets -> (pdfs com várias informações)
+
+
+#comando: alt + shift + K (atalhos das opções e digitação)
+
+
+
+# Operações Matemáticas Básicas -------------------------------------------
+
+
+
+#adição
+
+#subtração
+
+#divisão
+
+#multiplicação
+
+#exponenciação pode ser feita de dois jeitos com ** ou ^
+
+#resto de divisão %%
+
+
+
+
+# Criando variáveis -------------------------------------------------------
+
+
+
+Variavel <- amigão         #erro
+
+Variavel <- "amigão"
+
+x <- 120
+Variavel <- x**2
+Variavel
+
+y <- c(1:10)             #c funciona para criar um vetor
+y
+
+z <- 1:10
+z
+
+#devido ao padrão do R, é possível tornar um vetor uma função:
+
+c <- "amigão"  
+c                 #retorna "amigão"
+
+c(1,2,3)
+c                 #o que vai retornar?
+
+
+
