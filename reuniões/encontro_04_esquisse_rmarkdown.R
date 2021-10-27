549b1f4a25fb3d627cf189a303480f3807fafee2
unknown <OppenSocial@NoteOppen0831.OPORTUNIDADES.es>
2021-10-26 21:50:04 -0300

    atualização, escopo encontro 4 e novos contribuidores

---
+++ reuniões/encontro_04_esquisse_rmarkdown.R
@@ -0,0 +1,27 @@
+##############################
+#     chesteR - reunião 04   #
+##############################
+
+#install.packages("esquisse")
+library(esquisse)
+#install.packages("ggplot2")
+library(ggplot2)
+
+
+# Gráficos esquisse -------------------------------------------------------
+
+cars <- mtcars
+
+# addins ggplot2builder
+
+# gerar o código do gráfico e passar pro markdown
+
+
+# Rmarkdown ---------------------------------------------------------------
+
+
+
+
+
+
+
