6b7dafc8c7679589f9d4473e4401caac87cb0766
Gabrielforest <gabrielfreitaspereira@estudante.ufscar.br>
2022-06-26 22:31:21 -0300

    Primeiro encontro presencial Turma B, revisão de conceitos básicos (variáveis).

---
+++ reuniões/presencial_revisão_01_02_turma_B.R
@@ -0,0 +1,81 @@
+######################################################
+#     chesteR - reunião presencial 01-02 - Turma B   #
+######################################################
+
+# Revisão de conceitos básicos
+
+# variáveis ---------------------------------------------------------------
+
+# definindo a variável: 
+gabriel <- "ahufhasudfhasdhufauafhfasdpjfap"
+gabriel
+
+# o código a seguir apresenta erro, pois não definimos o valor da variável aaaaaaaaa
+dnd <- aaaaaaaaa
+
+# excluindo uma variável:
+
+
+# excluindo 2 variáveis:
+
+
+# excluindo todas as variáveis:
+
+
+
+# atalhos -----------------------------------------------------------------
+
+# alt - 
+# alt + setinha = vai pro fim da linha
+# ctrl + setinha = vai pro fim da palavra ou sinal
+# ctrl + shift + setinha = seleciona  
+
+
+# Boas práticas -----------------------------------------------------------
+
+# exemplo para não seguir:
+formula <- "dicc~dap*(b0+b1*(hi/ht)+b2*((hi/ht)^2)+b3*((hi/ht)^3)+b4*((hi/ht)^4)+
+b5*((hi/ht)^5))"
+
+# é ideal separar textos e/ou códigos extensos para facilitar a compreensão
+formula <- "dicc ~ dap * 
+              ( b0 
+              + b1 * (hi/ht) 
+              + b2 * ( (hi/ht) ^ 2) 
+              + b3 *( (hi/ht) ^ 3) 
+              + b4 *( (hi/ht) ^ 4) 
+              + b5 * ( (hi/ht) ^ 5 ) 
+              )"
+
+
+# operações ---------------------------------------------------------------
+
+aaaaaaaaa <- 1 + 1
+
+# agora que salvei a variável aaaaaaaaa, o código abaixo funcionará:
+dnd <- aaaaaaaaa
+
+# adição
+
+#ou sem espaçamento 
+
+#ou usando parênteses
+
+# subtração
+
+#ou sem espaçamento 
+
+#ou usando parênteses
+
+
+# divisão
+
+
+# multiplicação
+
+
+# exponenciação pode ser feita de dois jeitos com ** ou ^
+
+#ou
+
+# resto da divisão %%
