35f2266036c8113dce3173e86fd06b04a8301a3f
Gabrielforest <gabrielfreitaspereira@estudante.ufscar.br>
2022-06-29 23:00:21 -0300

    escopo do que será discutido

---
+++ reuniões/presencial_função_plot_01_turma_A.R
@@ -0,0 +1,42 @@
+######################################################
+#     chesteR - reunião presencial 01  -   Turma A   #
+######################################################
+
+# Gráficos
+
+# dados de CO2 que serão usados -------------------------------------------
+
+
+# histograma de absorção de CO2
+
+
+# criando janela gráfica --------------------------------------------------
+
+# linux
+
+# macintosh
+
+# windows
+
+
+# Gráfico de densidade (curva histograma) ---------------------------------
+
+
+
+# função plot
+
+
+# colocando título, alterando labels dos eixos, e ajustando limites
+
+
+
+# legenda por coordenada
+
+
+# função curve
+
+# isso é a mesma coisa que:
+
+# adicionando gráfico
+
+# legenda por coordenada
