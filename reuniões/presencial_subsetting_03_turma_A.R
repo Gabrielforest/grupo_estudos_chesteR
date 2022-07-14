4f9fd6a7822c4d2f859ca769938de270eb7b2c0e
Gabrielforest <gabrielfreitaspereira@estudante.ufscar.br>
2022-07-14 10:02:25 -0300

    escopo do que será discutido no encontro 03 presencial

---
+++ reuniões/presencial_subsetting_03_turma_A.R
@@ -0,0 +1,38 @@
+###################################################
+#   chesteR - reunião presencial 03 - Turma A     #
+###################################################
+
+# Subsetting
+
+x <- c( 2, 4, 3, 5 )
+
+# selecionando por posições 
+
+# índices duplicados
+
+# selecionando por exclusão de valores
+
+# ordenando dados (crescente)
+
+# decrescente
+
+# seleção por condição
+
+# equivalente a
+
+# definindo nomes, função setNames
+
+# selecionando colunas dados CO2
+
+# usando dados CO2, funções: colnames, rownames, colSums, rowSums, lapply
+
+# introdução tibble
+
+# amostrando dados da tabela
+
+# removendo colunas atribuindo nulo
+
+
+
+
+
