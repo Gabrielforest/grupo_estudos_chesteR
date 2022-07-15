2e7945506c9e32f3b18ce1fe5433c45f7f087dee
Gabrielforest <gabrielfreitaspereira@estudante.ufscar.br>
2022-07-15 10:54:06 -0300

    escopo do que será discutido no encontro prensencial 3 turma B

---
+++ reuniões/presencial_subsetting_personalizando_plot_03_turma_B.R
@@ -0,0 +1,45 @@
+###################################################
+#   chesteR - reunião presencial 03 - Turma B     #
+###################################################
+
+# Subsetting
+
+x <- c( 1, 2, 4, 3, 5 )
+
+# selecionando por posições 
+
+# índices duplicados
+
+# selecionando por exclusão de valores
+
+# ordenando dados (crescente)
+
+# decrescente
+
+# seleção por condição
+
+# equivalente a
+
+# definindo nomes, função setNames
+
+# selecionando colunas dados CO2
+
+# usando dados CO2, funções: colnames, rownames, colSums, rowSums, lapply
+
+# introdução tibble
+
+# amostrando dados da tabela de forma aleatória
+
+# removendo colunas atribuindo nulo
+
+
+# plot CO2 e alterando: ---------------------------------------------------
+
+
+# forma dos pontos
+# cor de fundo 
+# cor dos números dos eixos
+# cor do título dos eixos
+# cor do título principal
+# cor dos componentes gráficos
+
