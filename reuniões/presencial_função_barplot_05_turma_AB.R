48e6748039e90ee9d9fad8f4aaed1e97dbb95529
Gabrielforest <gabrielfreitaspereira@estudante.ufscar.br>
2022-08-04 22:19:14 -0300

    escopo do que será discutido na reunião 05 turma AB

---
+++ reuniões/presencial_função_barplot_05_turma_AB.R
@@ -0,0 +1,36 @@
+#' --- 
+#' title: Reunião presencial 04 - Turma AB - chesteR
+#' subtitle: Gráficos de barra
+#' author: Gabriel de Freitas Pereira
+#' date: Agosto de 2022
+#' ---
+
+#' dados Pinus taeda
+Loblolly
+
+#' separando sementes 319
+
+#' usando which
+
+#' usando subset
+
+#' comparando velocidade das funções
+
+#' criando tabela do Pinus 319 
+
+#' gráfico altura por idade
+
+#' cores rgb
+
+#' adicionando cores no gráfico
+
+#' adicionando texto
+
+#' diminuindo tamanho dos números
+
+#' adequando posição dos números
+
+#' adequando limites das barras
+
+
+
