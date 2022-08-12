636b7d166089b81efb93bda355760a1664dd0127
Gabrielforest <gabrielfreitaspereira@estudante.ufscar.br>
2022-08-12 12:51:10 -0300

    Escopo do que será discutido no encontro de hoje

---
+++ reuniões/presencial_manipulando_caracteres_06_turma_AB.R
@@ -0,0 +1,32 @@
+#' --- 
+#' title: Reunião presencial 06 - Turma AB - chesteR
+#' subtitle: Manipulação de caracteres
+#' author: Gabriel de Freitas Pereira
+#' date: Agosto de 2022
+#' ---
+
+#' operações com vetorização 
+
+#' contando número de caracteres
+
+#' colando caracteres com vetor
+
+#' lendo nomes de cada linha
+
+#' lendo nome de uma linha específica
+
+#' lendo conteúdo de uma linha específica de uma coluna específica
+
+#' utilizando `paste` em formatação de resultados, com vetorização
+
+#' utilizando separador específico
+
+#' combinando números e caracteres com comando paste
+
+#' criando tabela
+
+#' loop para colar os resultados de cada coluna
+
+#' fazendo o mesmo com tabela PlantGrowth
+
+
