9ddb20905bbd2d4e78295657a6191eca988eae34
Gabrielforest <gabrielfreitaspereira@estudante.ufscar.br>
2022-08-19 10:50:48 -0300

    escopo do que será discutido no encontro de hoje

---
+++ reuniões/presencial_agregando_informações_07_turma_AB.R
@@ -0,0 +1,33 @@
+#' --- 
+#' title: Reunião presencial 07 - Turma AB - chesteR
+#' subtitle: Agregando informações
+#' author: Gabriel de Freitas Pereira
+#' date: Agosto de 2022
+#' ---
+
+#' dados de laranjeiras
+
+#' renomeando dados
+
+#' qual a média de idade das laranjeiras em dias?
+
+#' qual a média de idade das laranjeiras em anos?
+
+#' arredondando resultado
+
+#' qual a média de circunferência das laranjeiras em centímetros?
+
+#' # Agregações --------------
+
+#' qual a média da circunferência por idade ?
+
+# é possível separar os dados para fazer as contas:
+
+# criar um loop para fazer isso 
+
+# ou usar a função aggregate
+
+
+
+#' qual a média de circunferência da árvore 4 ?
+
