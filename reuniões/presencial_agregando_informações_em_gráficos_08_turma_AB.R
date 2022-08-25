8f4784315c60b4b00f0622e94dfacb1d940c5805
Gabrielforest <gabrielfreitaspereira@estudante.ufscar.br>
2022-08-25 14:59:41 -0300

    escopo do que será discutido no encontro 08 turma AB

---
+++ reuniões/presencial_agregando_informações_em_gráficos_08_turma_AB.R
@@ -0,0 +1,35 @@
+#' --- 
+#' title: Reunião presencial 08 - Turma AB - chesteR
+#' subtitle: Agregando informações para gráficos e estatísticas
+#' author: Gabriel de Freitas Pereira
+#' date: Agosto de 2022
+#' ---
+
+#' pacotes que serão usados
+
+library( psych )
+library( ggpubr )
+
+#' dados que serão usados
+
+#' estatísticas dos dados
+
+#' estatísticas por grupos
+
+#' agrupando dados pela média de len por supp e dose
+
+#' criando gráfico de linhas de len por dose 
+
+#' criando gráfico de barras de len por dose 
+
+#' fazendo iniciar no 0
+
+#' acrescentando o número da média de cada dose em cima das barras
+
+
+# equivalente a reescrever:
+
+#' aumentando limites
+
+
+
