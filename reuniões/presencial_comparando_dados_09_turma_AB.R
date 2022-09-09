f9fe6f1b3c803938763ad85397f7eb9fb291538f
unknown <gabrielfreitaspereira@estudante.ufscar.br>
2022-09-09 08:31:58 -0300

    escopo do que será discutido encontro 09 turma AB

---
+++ reuniões/presencial_comparando_dados_09_turma_AB.R
@@ -0,0 +1,29 @@
+#' --- 
+#' title: Reunião presencial 09 - Turma AB - chesteR
+#' subtitle: Organizando dados + teste estatístico
+#' author: Gabriel de Freitas Pereira
+#' date: Setembro de 2022
+#' ---
+
+#' Gerando tabela que será usada
+
+
+#' criando coluna categórica dos dados e unificando dados de altura
+
+#' primeira alternativa
+
+#' segunda alternativa, usando pacote  
+
+
+#' comparando médias
+#' usando comandos básicos do R
+
+
+#' resultado: as médias..
+
+#' boxplot
+
+
+
+#' usando pacote ggpubr
+
